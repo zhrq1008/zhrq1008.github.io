a9285ed19fe72833c847e2f88b91bdbb0f4765a4
zhrq1008 <251869414@qq.com>
2016-09-27 15:53:19 +0800

    123

---
+++ data/userInfo.js
@@ -0,0 +1,37 @@
+
+/*个人信息说明，数据格式为对象
+	*   {
+	*       userName: @value String               *         姓名
+			userPortrait: @value String           *         头像
+	*       jobWant: @value String                *         求职意向
+	*       userQQ: @value String|Number          *         QQ号码
+	*       userEmail: @value String              *         邮箱地址
+	*       motto: @value String                  []         激励格言
+	*       userPhone: @value String|Number       *          手机号码
+	*       userOrignPlace: @value String         *         籍贯
+	*       userSeatPlace: @value String          *         所在地
+	*       userWeibo：@value String              []         微博地址
+	*       userAssessment: @value String         []        自我评价
+	*       userHobby: @value Array              []          爱好
+	*       userAward:@value Array               []          奖项
+	*
+	*
+	*   }
+*/
+
+var userInfo = {
+    userName: "张瑞谦" , 
+    userPortrait:"images/icon/zhrq.png",
+    jobWant:  "WEB前端开发工程师",
+    userQQ:  "251869414",
+    userEmail:   "251869414@qq.com",
+    motto:  "置气和悲伤的源头都在于自己，你不听不闻不看，选择权在你置气和悲伤的源头都在于自己，你不听不闻不看，选择权在你置气和悲伤的源头都在于自己，你不听不闻不看，选择权在你置气和悲伤的源头都在于自己，你不听不闻不看，选择权在你", 
+    userPhone: 15032829781,
+    userOrignPlace:  "河北省保定市" ,
+    userSeatPlace:  "北京市",
+//  userWeibo: "javascript@miaov.com",
+    userAssessment: "丰富的互联网工作经验，精通各种JS技巧与实现，为人和善。",   
+    userHobby: ["运动","游戏","电影"],
+    userAward: ["最佳前端架构奖"]
+}
+
